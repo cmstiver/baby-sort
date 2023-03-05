3cd89a3d45eb6958205e6045ab3226f861e4e02e
cmstiver <chayse.stiver@gmail.com>
2023-03-05 00:38:00 -0800

    edited test file

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 from baby_sort import CupGame, Stack
 
-cups = CupGame()
-
-cups_list = cups.create_cups(300)
+cups_list = CupGame.create_cups(300)
 stack = Stack()
 
-cups.baby_sort(cups_list, stack)
+CupGame.baby_sort(cups_list, stack)
